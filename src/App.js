d1eeed9198cdcc7b818ca159342c5453b16697ce
Alejae1998 <alejagr1998@gmail.com>
2023-01-09 11:48:54 -0800

    adding a file for audio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import './App.css';
 
 import Header from './components/Header/Header';
@@ -7,11 +6,16 @@ import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
 
 function App() {
+  let audio = new Audio('/public/farm.mp3');
+  const start = () => {
+    audio.play();
+  };
   return (
     <div className="App">
       <Header />
       <Main />
       <Footer />
+      <button onClick={start}>PLAY</button>
     </div>
   );
 }
