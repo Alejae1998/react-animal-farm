0b1338710c1a3b82bda2ea56366dedd6fc4ae089
Alejae1998 <alejagr1998@gmail.com>
2023-01-09 12:26:50 -0800

    adding a stretch goal

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
 
+import sound from './farm.mp3';
 function App() {
-  let audio = new Audio('/public/farm.mp3');
-  const start = () => {
+  const audio = new Audio(sound);
+  function playAudio() {
     audio.play();
-  };
+  }
   return (
     <div className="App">
       <Header />
+      <button onClick={playAudio}>PLAY</button>
       <Main />
       <Footer />
-      <button onClick={start}>PLAY</button>
     </div>
   );
 }
